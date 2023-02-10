e9ae4e2590bc0165566b99f5936da185329ff2d3
Ben <ben@ahnlab.io>
2023-02-10 11:44:31 +0000

    add

--- handler.py
+++ handler.py
@@ -1,16 +1,19 @@
 import os
+import logging
 import requests
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.INFO)
 
 
 def proxy(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
+    log.info(event)
     return request_rest_apigateway()
 
 
 def request_rest_apigateway():
     url = os.getenv("REST_API_GATEWAY_URL")
+    log.info("request rest api gateway %s", url)
     r = requests.get(url)
     return {"statusCode": r.status_code, "body": r.content}
